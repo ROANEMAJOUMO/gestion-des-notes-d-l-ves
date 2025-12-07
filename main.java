c675df73542400a828003cd88c9e95b2cd78da2f
unknown <carellemajoumo48@gmail.com>
2025-12-07 17:16:23 +0100

    Modification du message d'affichage

--- main.java
+++ main.java
@@ -4,10 +4,11 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("hello");
     Promotion promo=new Promotion("I1");
     Etudiant Karim=new Etudiant("chengan","Karim");
     Karim.addResultat("POO",new Note());
-    Karim.addResultat("Reseaux",new Note(18.0));
+    Karim.addResultat("mathematiques",new Note(18.0));
     promo.addEtudiant(Karim);
 
     promo.afficherEtudiantsResultats();
